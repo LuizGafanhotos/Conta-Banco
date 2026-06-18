d90dcedebb95ad3cca286432dfc69ebe46d8cc59
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2026-06-18 05:38:25 -0300

    adicionando index

--- index.php
+++ index.php
@@ -8,6 +8,17 @@
     <title>Conta bancária</title>
 </head>
 <body>
-    
+    <?php 
+        $saque = $_REQUEST['saque'];
+    ?>
+    <main>
+        <h1>Saque eletronico</h1>
+        <form action="<?=$_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="saque">Qual valor você quer sacar? <sup>*</sup></label>
+            <input type="number" name="saque" id="saque" required step="5" value="<?=$saque?>">
+            <p>Notas disponíveis: R$ 100, R$ 50, R$ 10, R$ 5</p>
+
+        </form>
+    </main>
 </body>
 </html>
